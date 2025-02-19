3c978885b805338cdd8991b3ebbc4a0f73331070
Genevieve <awavieve@gmail.com>
2025-02-19 10:56:31 -0500

    Add anticipated show api

--- week7/testapp-wk7/components/trakt/api.js
+++ week7/testapp-wk7/components/trakt/api.js
@@ -4,9 +4,10 @@ const trakt = "https://api.trakt.tv"; //base URL for any Trakt API requests
  * Functions for Trakt API requests.
  */
 
+// Movie
 //Function to retrieve a list of trending movies.
 async function getTrendingMovies() {
-  let reqUrl = `${trakt}/movies/trending`; // the request url to get trending movies
+  let reqUrl = `${trakt}/movies/trending?extended=full`; // the request url to get trending movies
   let response = await fetch(
     reqUrl,
     {
@@ -35,7 +36,26 @@ async function getStudiosByMovieId(imdbId) {
   return await response.json();
 }
 
+
+// Show
+async function anticipatedShows() {
+  let reqUrl = `${trakt}/shows/anticipated?extended=full?page=1&limit=15`; // the request url to get shows
+  let response = await fetch(
+    reqUrl,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "trakt-api-version": 2,
+        "trakt-api-key": process.env.TRAKT_CLIENT_ID
+      }
+    }
+  );
+  return await response.json();
+}
+
+
 module.exports = {
   getTrendingMovies,
-  getStudiosByMovieId
+  getStudiosByMovieId,
+  anticipatedShows
 };
--- week7/testapp-wk7/index.js
+++ week7/testapp-wk7/index.js
@@ -17,16 +17,27 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 //PAGE ROUTES
+
+// Movie API
 app.get("/", async (request, response) => {
   let movies = await trakt.getTrendingMovies();
   console.log(movies);
   response.render("index", { trendingMovies: movies});
 });
+
 app.get("/movie/:imdbId/studios", async (request, response) => {
   console.log(request.params.imdbId); // req.params conatins any url placeholders (e.g imdbid is a placeholder named imdbID)
   let studios = await trakt.getStudiosByMovieId(request.params.imdbId);
   console.log(studios);
   response.render("studios", {movieStudios: studios});
+});
+
+
+// Show API
+app.get("/shows", async (request, response) => {
+  let shows = await trakt.anticipatedShows();
+  console.log(shows);
+  response.render("show", { shows: shows});
 });
 
 //set up server listening
